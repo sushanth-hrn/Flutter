8bc62dc85d383f841b72af13da37b4a54105927d
Sushanth <sushanth.nukala14@gmail.com>
2021-05-16 12:58:15 +0530

    SignOut setup

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:food_aid/services/auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 class Home extends StatelessWidget {
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    return Text('Home');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        actions: <Widget>[
+          FlatButton.icon(
+            onPressed: () async {
+              await _auth.signOut();
+            },
+            icon: Icon(Icons.person),
+            label: Text('Sign Out'),
+          ),
+        ],
+      ),
+    );
   }
 }
